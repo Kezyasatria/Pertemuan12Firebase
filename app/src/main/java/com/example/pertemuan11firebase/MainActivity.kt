5e4d45eae052c193f7cd6557321f5b515a2b7641
Kezyasatria <kezyasatria10@gmail.com>
2023-08-11 20:05:40 +0700

    Initial commit

---
+++ app/src/main/java/com/example/pertemuan11firebase/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.app.pertemuansebelas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.pertemuan11firebase.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/example/pertemuan11firebase/model/Mahasiswa.kt
@@ -0,0 +1,8 @@
+package com.example.pertemuan11firebase.model
+
+data class Mahasiswa(
+    val id : String =" ",
+    val nim : String = " ",
+    val nama : String = " ",
+    val telepon : String = " "
+)
